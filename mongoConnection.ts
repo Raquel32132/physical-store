5fdd17aee60b301b4596d54ee3e43fa6b93b7fb9
Raquel Ramos Backes <raquelrbackes@gmail.com>
2024-10-22 11:46:15 -0300

    feat: updating connectDatabase function on mongoConnection

--- mongoConnection.ts
+++ mongoConnection.ts
@@ -5,6 +5,12 @@ dotenv.config({ path: '../.env' });
 
 const db = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`;
 
-mongoose.connect(db)
-  .then(() => console.log('Database connection successful!'))
-  .catch((err) => console.error('Database connection error:', err));
+export const connectDatabase = async () => {
+  try {
+    await mongoose.connect(db);
+    console.log('Database connection successful!');
+  } catch (err) {
+    console.error('Database connection error: ', err);
+    process.exit(1);
+  }
+}
