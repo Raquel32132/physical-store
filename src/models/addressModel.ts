3568f099c1b9c9dc0715b20f1efd42ae3d3198c9
Raquel Ramos Backes <raquelrbackes@gmail.com>
2024-10-22 14:27:20 -0300

    feat: updated address model with mongoose

--- src/models/addressModel.ts
+++ src/models/addressModel.ts
@@ -1,4 +1,6 @@
-export interface Address {
+import mongoose, { Schema, Document } from 'mongoose';
+
+interface AddressProps extends Document {
   street: string;
   number: string;
   complement?: string;
@@ -8,3 +10,16 @@ export interface Address {
   postalCode: string;
   country: string;
 }
+
+const addressSchema: Schema = new Schema({
+  street: { type: String, required: true },
+  number: { type: String, required: true },
+  complement: { type: String, required: false },
+  neighborhood: { type: String, required: true },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  postalCode: { type: String, required: true },
+  country: { type: String, required: true }
+});
+
+export const Address = mongoose.model<AddressProps>('Address', addressSchema);
