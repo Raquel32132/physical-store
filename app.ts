7d1c85c8ba06498381dfbdb706a3ba255503f7c8
Raquel Ramos Backes <raquelrbackes@gmail.com>
2024-10-23 08:34:32 -0300

    feat: calling post route for create store in app

--- app.ts
+++ app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import storeRouter from './src/routes/storeRoute';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api/v1/stores', storeRouter);
 
 export default app;
