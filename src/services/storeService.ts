c66bc33c6b1ea74e5e09ad72796a108f338c3bcf
Raquel Ramos Backes <raquelrbackes@gmail.com>
2024-10-30 13:45:10 -0300

    feat: fetching address data when update postal code store

--- src/services/storeService.ts
+++ src/services/storeService.ts
@@ -1,4 +1,4 @@
-import { Address } from "../models/addressModel";
+import { Address, AddressProps } from "../models/addressModel";
 import { Store, StoreProps } from "../models/storeModel"
 import { getAddressByPostalCode, getCoordinates } from "./addressService";
 
@@ -14,7 +14,7 @@ interface storeDataProps {
     number: string;
     complement?: string;
   }
-}
+};
 
 export const createStore = async (storeData: storeDataProps): Promise<StoreProps> => {
   const addressData = await getAddressByPostalCode(storeData.address.postalCode);
@@ -46,12 +46,35 @@ export const getAllStores = async (): Promise<StoreProps[]> => {
 
 export const getStoreById = async (id: string): Promise<StoreProps | null> => {
   return Store.findById(id).populate('address');
-}
+};
 
 export const updateStore = async (id: string, updateData: Partial<StoreProps>): Promise<StoreProps | null> => {
+  if (updateData.address) {
+    const { postalCode, number, complement } = updateData.address;
+
+    if (postalCode) {
+      const addressData = await getAddressByPostalCode(postalCode);
+      const formattedAddress = `${addressData.logradouro}, ${addressData.uf}, ${addressData.localidade}`
+      const coordinates = await getCoordinates(formattedAddress);
+
+      updateData.address = {
+        street: addressData.logradouro,
+        neighborhood: addressData.bairro,
+        city: addressData.localidade,
+        state: addressData.estado,
+        postalCode: postalCode,
+        number: number,
+        complement: complement,
+        country: 'Brasil',
+        latitude: coordinates.latitude,
+        longitude: coordinates.longitude
+      } as AddressProps;
+    }
+  }
+
   return Store.findByIdAndUpdate(id, updateData, { new: true }).populate('address');
-}
+};
 
 export const deleteStore = async (id: string): Promise<StoreProps | null> => {
   return Store.findByIdAndDelete(id);
-}
+};
