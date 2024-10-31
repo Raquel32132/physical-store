5a5a81b7e45696605942d7e55c2b76477fa300e6
Raquel Ramos Backes <raquelrbackes@gmail.com>
2024-10-31 14:35:22 -0300

    feat: creating logger

---
+++ src/utils/logger.ts
@@ -0,0 +1,26 @@
+import winston from "winston";
+import { Request } from 'express';
+
+const { combine, timestamp, json } = winston.format;
+
+export const logger = winston.createLogger({
+  level: 'info',
+  format: combine (
+    timestamp(),
+    json()
+  ),
+  transports: [
+    new winston.transports.Console()
+  ]
+});
+
+export const logWithCorrelationId = (req: Request) => {
+  const correlationId = req.headers['x-correlation-id'];
+
+  return {
+    info: (message: string) => logger.info({ message, correlationId }),
+    warn: (message: string) => logger.warn({ message, correlationId }),
+    error: (message: string) => logger.error({ message, correlationId }),
+  };
+};
+
